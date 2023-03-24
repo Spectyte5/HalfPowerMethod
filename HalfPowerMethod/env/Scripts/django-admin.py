ddd835478eafc9834aa9848bdd1b602a69e3d75a
Rafał Wróbel <102460245+Spectyte5@users.noreply.github.com>
2023-03-24 19:56:05 +0100

    Delete HalfPowerMethod/env directory

--- HalfPowerMethod/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\rafal\source\repos\HalfPowerMethod\HalfPowerMethod\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
